f63955f560269055b2777bf4c632cf7f5919e4bd
Michał Sieczkowski <mikee2509@gmail.com>
2017-04-25 02:05:38 +0200

    Replace fixed length buffers with dynamic memmory allocation

--- filter.asm
+++ filter.asm
@@ -1,8 +1,6 @@
 .data
 		
 header:		.space 56
-buffer:		.space 9000
-outbuffer:	.space 9000
 size:		.space 4  # space for input image bitmap size in bytes
 width:		.space 4  # space for image width in pixels
 height:		.space 4  # space for image height in pixels
@@ -10,8 +8,8 @@ height:		.space 4  # space for image height in pixels
 welcomeMsg:	.asciiz "High Pass / Low Pass filter\n   Michal Sieczkowski 04.2017\n\n"
 openedMsg:	.asciiz "File opened\n   Processing...\n"
 errorMsg:	.asciiz "Error opening/reading file\n"
-inFileName:	.asciiz "aaa2.bmp"
-outFileName:	.asciiz "bbb2debug.bmp"
+inFileName:	.asciiz "input_big2.bmp"
+outFileName:	.asciiz "input_big21.bmp"
 
 
 debugWidth: 	.asciiz "Width: "
@@ -90,6 +88,7 @@ main:
 	##############################################
 	
 ##DEBUG----------------------------------
+
 	la $a0, debugWidth 	
 	li $v0, 4
 	syscall
@@ -115,7 +114,8 @@ main:
 	li $v0, 4
 	syscall
 
-##---------------------------------------
+##DEBUG-END----------------------------------
+
 
 	# Write header to output file:
 	move $a0, $t1        # output file descriptor 
@@ -123,6 +123,18 @@ main:
 	li   $a2, 54         # header length
 	li   $v0, 15         # write to file
 	syscall
+	
+	# Allocate in buffer space:
+	move $a0, $s0        # number of bytes to allocate 
+	li   $v0, 9          # allocate
+	syscall
+	move $t5, $v0	     # address of allocated memmory
+	
+	# Allocate out buffer space:
+	move $a0, $s0        # number of bytes to allocate 
+	li   $v0, 9          # allocate
+	syscall
+	move $t7, $v0	     # address of allocated memmory
 	
 	# Calculate row size:
 	mul $s3, $s1, 24	# The formula is:	
@@ -138,31 +150,57 @@ main:
 	sub $t9, $s3, $t2
 	### $t9 now holds padding
 
-
-	###### s0 s1 s2 will be reused from now on ######
-	
-	li $s0, 1 # center
-	li $s1, 1 # edge
-	li $s2, 1 # corner
-	mul $s6, $s1, 4
-	mul $s7, $s2, 4
-	add $s6, $s6, $s7
-	add $s6, $s6, $s0  # sum of factors 
 	
 	
 readToBuffer:
 	# Read to buffer:
   	move $a0, $t0     	# input file descriptor
-	la   $a1, buffer   	# input buffer address
-  	li   $a2, 9000		# max num of characters to read
+	move $a1, $t5   	# input buffer address
+  	move $a2, $s0		# max num of characters to read
+  	li   $v0, 14  	 	# read from file
+  	syscall	
+  	move $t2, $v0		
+  	bltz $t2, fileError
+###----------------------------  	
+  	move $a0, $t0      # input file descriptor to close
+	li   $v0, 16       # close file 
+	syscall
+	
+	la $a0, inFileName	# file name
+	li $a1, 0		# open for reading only
+	li $a2, 0		# mode is ignored
+	li $v0, 13		# open file
+	syscall
+
+	move $t0, $v0 		# save file descriptor in $t0 (negative if error)
+	bltz $t0, fileError	# exit program if error occured
+	
+  	move $a0, $t0     	# input file descriptor
+	la   $a1, header+2   	# input buffer address
+  	li   $a2, 54		# num of characters to read
+  	li   $v0, 14  	 	# read from file
+  	syscall
+  	
+  	move $a0, $t0     	# input file descriptor
+	move $a1, $t7   	# input buffer address
+  	move $a2, $s0		# max num of characters to read
   	li   $v0, 14  	 	# read from file
   	syscall	
   	move $t2, $v0		
   	bltz $t2, fileError
   	
-  	# ok wczytałem t2 bajtów potem sprawdze czy to tyle (t2<9000) czy trzeba jeszcze, 
-  	# processuje pierwszys rząd
-  
+ ###-----------------------------
+  	
+  	###### s0 s1 s2 will be reused from now on ######
+	
+	li $s0, 14 # center
+	li $s1, -1 # edge
+	li $s2, -1 # corner
+	mul $s6, $s1, 4
+	mul $s7, $s2, 4
+	add $s6, $s6, $s7
+	add $s6, $s6, $s0  # sum of factors 
+  	 
   
   	move $t3, $s3
 	lw $t8, size
@@ -177,15 +215,16 @@ nextRow:
   			
 nextByte:
 	
-	lbu $s4, buffer($t3)
+	add $t6, $t5, $t3
+	lbu $s4, ($t6)
 	mul $s4, $s4, $s1 	# middle left 
 	move $s5, $s4
 	
-	lbu $s4, buffer+3($t3)
+	lbu $s4, 3($t6)
 	mul $s4, $s4, $s0	# center
 	add $s5, $s5, $s4
 	
-	lbu $s4, buffer+6($t3)
+	lbu $s4, 6($t6)
 	mul $s4, $s4, $s1	# middle right
 	add $s5, $s5, $s4
 	
@@ -193,15 +232,16 @@ nextByte:
 	
 	sub $t3, $t3, $s3
 	
-	lbu $s4, buffer($t3)
+	add $t6, $t5, $t3
+	lbu $s4, ($t6)
 	mul $s4, $s4, $s2 	# bottom left
 	add $s5, $s5, $s4
 	
-	lbu $s4, buffer+3($t3)
+	lbu $s4, 3($t6)
 	mul $s4, $s4, $s1	# bottom center
 	add $s5, $s5, $s4
 	
-	lbu $s4, buffer+6($t3)
+	lbu $s4, 6($t6)
 	mul $s4, $s4, $s2	# bottom right
 	add $s5, $s5, $s4
 	
@@ -210,15 +250,16 @@ nextByte:
 	add $t3, $t3, $s3
 	add $t3, $t3, $s3
 	
-	lbu $s4, buffer($t3)
+	add $t6, $t5, $t3
+	lbu $s4, ($t6)
 	mul $s4, $s4, $s2 	# top left
 	add $s5, $s5, $s4
 	
-	lbu $s4, buffer+3($t3)
+	lbu $s4, 3($t6)
 	mul $s4, $s4, $s1	# top center
 	add $s5, $s5, $s4
 	
-	lbu $s4, buffer+6($t3)
+	lbu $s4, 6($t6)
 	mul $s4, $s4, $s2	# top right
 	add $s5, $s5, $s4
 	
@@ -230,7 +271,8 @@ nextByte:
 	
 	# Store this value in outbuffer:
 	sub $t3, $t3, $s3
-	sb $s4, outbuffer+3($t3)
+	add $t6, $t7, $t3
+	sb $s4, 3($t6)
 	
 	
 	# Increment pixel
@@ -245,9 +287,11 @@ nextByte:
 	
 	
 	# Write header to output file:
+	lw $t8, size
+	
 	move $a0, $t1        # output file descriptor 
-	la   $a1, outbuffer  # address of buffer from which to write
-	li   $a2, 9000       # buffer length
+	move $a1, $t7	     # address of buffer from which to write
+	move $a2, $t8        # buffer length
 	li   $v0, 15         # write to file
 	syscall
 	
